b9f743a869ad71f58f4c5b59e65c59462555fd42
Samuel Cedarbaum <scedarbaum@gmail.com>
2024-08-28 02:11:37 -0400

    Use rate limit prefix for global limit

--- utils/RateLimiting.ts
+++ utils/RateLimiting.ts
@@ -3,17 +3,20 @@ import { Redis } from "@upstash/redis";
 import { NextApiRequest } from "next";
 import requestIp from "request-ip";
 
+const RATE_LIMIT_PREFIX = "cedarbaum.io";
+
 const globalRequestLimit = parseInt(process.env.DAILY_API_LIMIT!);
 const globalRateLimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.fixedWindow(globalRequestLimit, "24 h"),
+  prefix: RATE_LIMIT_PREFIX,
 });
 
 const perIpPerMinRequestLimit = parseInt(process.env.PER_IP_PER_MIN_LIMIT!);
 const ipLimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(perIpPerMinRequestLimit, "1 m"),
-  prefix: "cedarbaum.io",
+  prefix: RATE_LIMIT_PREFIX,
 });
 
 export default async function apiQuotaAvailable(req: NextApiRequest) {
