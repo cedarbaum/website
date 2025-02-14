1567403b13f4f522c1ab27d2eaa7e534101cdf36
Samuel Cedarbaum <scedarbaum@gmail.com>
2025-02-14 15:22:27 -0500

    Improve chat height on laptops

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <main className="w-full h-full">
         <div className="bg-primary dark:bg-black relative h-full w-full flex flex-col md:mx-auto">
           <div className="md:p-8 relative h-full w-full md:mx-auto flex flex-col justify-center items-center">
-            <div className="w-full h-full md:h-[80%] md:max-h-[800px] md:max-w-md overflow-visible">
+            <div className="w-full h-full md:h-[90%] md:max-h-[1024px] md:max-w-md overflow-visible">
               <Chat />
             </div>
           </div>
